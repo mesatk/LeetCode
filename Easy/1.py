a6d1f0913dc743203a9eb3b47ae24414d5a3406c
esatkiraz <esat.kiraz@bilgeadam.com>
2021-06-28 13:39:00 +0300

    Create 1.py
    1. Two Sum

---
+++ Easy/1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}        # Create dictionary
+        for i, num in enumerate(nums):      # keep number and index
+            if (target - num) in dic:       # if the completing number is in the dic
+                return [dic[target - num], i]   # first the index of the number in the dic
+            else:   
+                dic[num] = i    # if the completing number is not in the dic, add
