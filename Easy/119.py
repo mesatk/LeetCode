20b0b66622500a9565ece3e9d4d3e6afd01ecbba
esatkiraz <esat.kiraz@bilgeadam.com>
2021-06-28 22:06:27 +0300

    119. Pascal's Triangle II

---
+++ Easy/119.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        pasq = [1]
+        
+        while rowIndex > 0:
+            head = 0
+            arr = [1]
+            while head + 1 < len(pasq):
+                arr.append(pasq[head] + pasq[head + 1])
+                head += 1
+            arr.append(1)
+            pasq = arr
+            rowIndex -= 1
+        
+        return pasq
