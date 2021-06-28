b94b7a1b8db458535c51585b2376d52bb7a4f609
esatkiraz <esat.kiraz@bilgeadam.com>
2021-06-28 16:59:13 +0300

    35. Search Insert Position

--- Easy/35.py
+++ Easy/35.py
@@ -7,3 +7,22 @@ class Solution(object):
             else:
                 index += 1
         return index
+
+
+
+####    Binary Search    ####
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+        
+        while low <= high:
+            mid = low + (high - low) / 2
+            
+            if(nums[mid] < target):
+                low += 1
+            else:
+                high -= 1
+        
+        return low
