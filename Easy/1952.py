d4ab63223d113437255ec5ea8a76d2645d6b9276
Muhammet Esat Kiraz <muhammetesat.kiraz@hotmail.com>
2021-08-07 18:35:20 +0300

    1952. Three Divisors

---
+++ Easy/1952.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isThree(self, n):
+        count = 0
+        for i in range(1,n+1):
+            if n % i == 0:
+                count += 1
+                if count > 3:
+                    return False
+        if count == 3:
+            return True
+        else:
+            return False
